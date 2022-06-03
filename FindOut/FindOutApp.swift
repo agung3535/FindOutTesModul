12e6dd7d4c4fba1674b1c53888c2c08669d4e1b7
agung <agungdwisaputra0808@gmail.com>
2022-06-03 14:02:29 +0700

    Initial Commit

---
+++ FindOut/FindOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindOutApp.swift
+//  FindOut
+//
+//  Created by Putra on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FindOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindOutUITests/FindOutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FindOutUITestsLaunchTests.swift
+//  FindOutUITests
+//
+//  Created by Putra on 03/06/22.
+//
+
+import XCTest
+
+class FindOutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
